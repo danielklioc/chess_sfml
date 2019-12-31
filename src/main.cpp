5ff0dfe684d47224208ed970aaaea191416e1c87
danielklioc <danielklioc@gmail.com>
2019-12-31 20:07:29 +0200

    added options
    - figures after have been defeated, removed from board
    - move back possible with backspace

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ Vector2f offset(28,28);
 Sprite figures[32]; //figures
 std::string position="";
 
+// Standard 8x8 board representation with figures weights
 int board[8][8] = 
     {-1,-2,-3,-4,-5,-3,-2,-1,
      -6,-6,-6,-6,-6,-6,-6,-6,
@@ -19,14 +20,20 @@ int board[8][8] =
       6, 6, 6, 6, 6, 6, 6, 6,
       1, 2, 3, 4, 5, 3, 2, 1};
 
+/*
+ * convert figure position from vector to board representation
+ */
 std::string toChessNote(Vector2f p)
 {
     std::string s = "";
-  s += char(p.x/size+97);
-  s += char(7-p.y/size+49);
-  return s;
+    s += char(p.x/size+97);
+    s += char(7-p.y/size+49);
+    return s;
 }
 
+/*
+ * move figure to coordinated
+ */
 Vector2f toCoord(char a,char b)
 {
    int x = int(a) - 97;
@@ -34,6 +41,10 @@ Vector2f toCoord(char a,char b)
    return Vector2f(x*size,y*size);
 }
 
+/*
+ * check if figure have been defeated
+ * remove from board
+ */
 void move(std::string str)
 {
     Vector2f oldPos = toCoord(str[0],str[1]);
@@ -43,7 +54,7 @@ void move(std::string str)
      if (figures[i].getPosition()==newPos) figures[i].setPosition(-100,-100);
         
     for(int i=0;i<32;i++)
-     if (figures[i].getPosition()==oldPos) figures[i].setPosition(newPos);
+        if (figures[i].getPosition()==oldPos) figures[i].setPosition(newPos);
 
     //castling       //if the king didn't move
     if (str=="e1g1") if (position.find("e1")==-1) move("h1f1"); 
@@ -52,6 +63,12 @@ void move(std::string str)
     if (str=="e8c8") if (position.find("e8")==-1) move("a8d8");
 }
 
+std::string postion = "";
+
+/*
+ * Calculate figures rectangule size
+ * Set position of each figure
+ */
 void loadPosition()
 {
     int k=0;
@@ -66,19 +83,27 @@ void loadPosition()
        figures[k].setPosition(size*j,size*i);
        k++;
      }
+
+    for(int i = 0; i<position.length(); i+=5)
+        move(position.substr(i,4));
 }
 
 
 int main()
 {
-    RenderWindow window(VideoMode(453, 454), "The Chess! (press SPACE)");
+    //RenderWindow window(VideoMode(594, 594), "The Chess! (press SPACE)");
+    RenderWindow window(VideoMode(454, 453), "The Chess! (press SPACE)");
 
     Texture t1,t2;
     t1.loadFromFile("images/figures.png"); 
     t2.loadFromFile("images/board0.png");
+    //t2.loadFromFile("images/bandw_chessboard.png");
 
     Sprite s(t1);
     Sprite sBoard(t2);
+
+    Vector2f oldPos, newPos;
+    std::string str;
 
     for(int i=0;i<32;i++) figures[i].setTexture(t1);
 
@@ -98,6 +123,14 @@ int main()
             if(e.type == Event::Closed)
                 window.close();
 
+            //move back
+            if(e.type == Event::KeyPressed)
+                if(e.key.code == Keyboard::BackSpace )
+                {
+                    position.erase(position.length() - 6,5);
+                    loadPosition();
+                }
+
             // drag and drop figures
             if(e.type == Event::MouseButtonPressed)
                 if(e.key.code == Mouse::Left)
@@ -108,18 +141,28 @@ int main()
                             n = i;
                             dx = pos.x - figures[i].getPosition().x;
                             dy = pos.y - figures[i].getPosition().y;
+                            oldPos = figures[i].getPosition();
                         }
 
             if(e.type == Event::MouseButtonReleased)
                 if(e.key.code == Mouse::Left)
+                {
                     isMove = false;
+                    Vector2f p = figures[n].getPosition() + Vector2f(size/2,size/2);
+                    Vector2f newPos = Vector2f( size*int(p.x/size), size*int(p.y/size));
+                    str = toChessNote(oldPos) + toChessNote(newPos);
+                    move(str);
+                    position += str + " ";
+                    std::cout<<str<<std::endl;
+                    figures[n].setPosition(newPos);
+                }
         }
 
         if (isMove) figures[n].setPosition(pos.x-dx, pos.y-dy);
 
-    	window.clear();
-    	window.draw(sBoard);
-    	for(int i =0; i<32; i++) window.draw(figures[i]);
-    	window.display();
+        window.clear();
+        window.draw(sBoard);
+        for(int i =0; i<32; i++) window.draw(figures[i]);
+        window.display();
     }
 }
